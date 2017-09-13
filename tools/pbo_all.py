0d1ba7ec6932b1612e3e3e0e69fbf2494f52de0d
Schwaggot <tom.ryan@posteo.de>
2017-09-13 21:18:34 +0200

    FIXED: replaced dynamic paths with hardcoded paths

--- tools/pbo_all.py
+++ tools/pbo_all.py
@@ -6,7 +6,7 @@ from shutil import move
 
 
 template_directory = '..'
-release_directory = 'release'
+release_directory = 'kellerkompanie-template/release'
 filebank_exe = 'FileBank.exe'
 
 
@@ -15,7 +15,7 @@ if __name__ == '__main__':
 
     filenames = glob.glob('kellerkompanie_zeus.*')
     filenames_count = len(filenames)
-    print 'Found', filenames_count, 'files inside', template_directory
+    print ('Found', filenames_count, 'files inside', template_directory)
 
     for filename in filenames:
         #print filename
@@ -25,9 +25,9 @@ if __name__ == '__main__':
         #print filebank_exe, '"' + os.path.abspath(filename) + '"'
         #subprocess.check_call([filebank_exe, '"' + os.path.abspath(filename) + '"'])
         #command = './d/SteamLibrary/steamapps/common/Arma\ 3\ Tools/FileBank/FileBank.exe -dst ../' + release_directory + ' ../' + filename
-        release_abs = os.path.abspath("../release").replace(" ", "\ ")
-        filename_abs = os.path.abspath(filename).replace(" ", "\ ")
-        command = '/d/SteamLibrary/steamapps/common/Arma\ 3\ Tools/FileBank/FileBank.exe -dst '  + release_abs + " source " + filename_abs
+        #release_abs = os.path.abspath("release").replace(" ", "\ ")
+        filename_abs = os.path.abspath(filename)#.replace("\\", "/")
+        command = '"D:\\SteamLibrary\\steamapps\\common\\Arma 3 Tools\\FileBank\\FileBank.exe" -dst "C:\\Users\\Alexander\\Documents\\Arma 3 - Other Profiles\\Schwaggot\\mpmissions\\kellerkompanie-template\\release" "' + filename_abs + '"'
               
-        print command
-        subprocess.call(command, shell=True)
+        print (command)
+        subprocess.call(command)
